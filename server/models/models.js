7158c8af836f9ef30993b8c31d24a29e48c8f3c6
Daniel <daniel.hope@sky.uk>
2021-11-08 13:36:38 +0000

    basic models

---
+++ server/models/models.js
@@ -0,0 +1,47 @@
+// get the seq package
+const Sequelize = require("sequelize");
+
+// model = outline of the data we'll store against an entity
+const user_model = {
+    username: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    password: {
+
+    }
+};
+const basket_model = {
+};
+const basket_items_model = {
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+}
+const products_model = {
+    title: {
+        type: Sequelize.STRING,
+    },
+    price : {
+        type: Sequelize.INTEGER
+    },
+    description: {
+        type: Sequelize.STRING
+    },
+    image: {
+        type: Sequelize.STRING
+    }
+}
+const category_model = {
+    title: {
+        type: Sequelize.STRING
+    }
+}
+
+module.exports = { user_model, codeModel };
