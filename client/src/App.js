008487d601e9a0e1d4c2a519c4a8c6374963b80d
Theo Anderson <euechinoidea@gmail.com>
2021-11-09 10:02:38 +0000

    product row skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import {products} from './products';
 const ProductRow = ({product}) => {
   console.log(product);
   return (
-    <div className="flextest">
+    <>
       <div className="product-image-container">
         <img src={product.image} alt={product.title}></img>
       </div>
       <div className="product-description-container">
         <h2>{product.title}</h2>
       </div>
-    </div>
+    </>
   )
 }
 
@@ -19,11 +19,13 @@ function App() {
   console.log(products);
   return (
     <div className="App">
-      <h1>Hello world :)</h1>
-      {products.map(product => {
-        return <ProductRow key={product.title} product={product}></ProductRow>
-      })
-      }
+      {/* <h1>Hello world :)</h1> */}
+      <div className="flextest">
+        {products.map(product => {
+          return <ProductRow key={product.title} product={product}></ProductRow>
+        })
+        }
+      </div>
     </div>
   );
 }
