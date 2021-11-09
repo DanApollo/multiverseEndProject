cf61cd72c6b502e09facee039d38327d87103f96
Rafael Barros Parigi <74794777+rafaelparigi@users.noreply.github.com>
2021-11-09 09:44:11 +0000

    Update indexRouter.js

--- server/routes/indexRouter.js
+++ server/routes/indexRouter.js
@@ -3,15 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const usersRouter = require('./usersRoutes');
-const basketsRouter = require('./basketRoutes');
-const basketItemsRouter = require('./basketItemRoutes');
+const basketsRouter = require('./basketsRoutes');
+const basketItemsRouter = require('./basketItemsRoutes');
 const productsRouter = require('./productsRoutes');
 const categoriesRouter = require('./categoriesRoutes');
 
 router.use("/api/users", usersRouter);
-router.use("/api/basket", basketsRouter);
+router.use("/api/baskets", basketsRouter);
 router.use("/api/basket-items", basketItemsRouter);
-router.use("/api/product", productsRouter);
-router.use("/api/category", categoriesRouter);
+router.use("/api/products", productsRouter);
+router.use("/api/categories", categoriesRouter);
 
 module.exports = router;
