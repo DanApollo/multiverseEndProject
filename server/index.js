a11e69ef3037a8a53990e21a16eea14364c13efe
Theo Anderson <euechinoidea@gmail.com>
2021-11-10 10:18:14 +0000

    test skeleton implemented

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-const { sequelize } = require("./connect");
 const routes = require("./routes/indexRouter.js");
 const cors = require("cors");
 const app = express();
@@ -12,17 +11,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes);
 
-
-async function start() {
-    await sequelize.sync({
-        logging: false,
-    });
-}
-
-start()
-    .then(() => console.log("Sequelize connected"))
-    .catch((e) => console.log(`Caught error: ${e}`));
-
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
+module.exports = app;
---
+++ server/start.js
@@ -0,0 +1,21 @@
+const app = require('./index') // the actual Express application
+const { sequelize } = require("./connect");
+const http = require('http')
+
+const PORT = 3001;
+
+const server = http.createServer(app)
+
+async function start() {
+    await sequelize.sync({
+        logging: false,
+    });
+}
+
+start()
+    .then(() => console.log("Sequelize connected"))
+    .catch((e) => console.log(`Caught error: ${e}`));
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
