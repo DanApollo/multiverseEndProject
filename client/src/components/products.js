1f9da56a9c0b0eb5c10dcacd0e920fd97c23f6cd
Theo Anderson <euechinoidea@gmail.com>
2021-11-11 22:59:30 +0000

    Revert "Added ability for admins to update descriptions"
    This reverts commit 0605ac615beacb1de29cbd0833d4ced8517eb3db.

--- client/src/components/products.js
+++ client/src/components/products.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -6,16 +5,15 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-const ProductRow = ({product, showDescription}) => {
+const ProductRow = ({product}) => {
     //console.log(product);
     return (
       <div className="flextest">
         <div className="product-image-container">
-          <img src={product?.image} alt={product?.title}></img>
+          <img src={product.image} alt={product.title}></img>
         </div>
         <div className="product-description-container">
-          <h2>{product?.title}</h2>
-          {showDescription && product?.description}
+          <h2>{product.title}</h2>
         </div>
       </div>
     )
@@ -62,40 +60,13 @@ const CategorySelect = ({categories, category, setCategory}) => {
 <MenuItem value={3}>Thirty</MenuItem>
 */
 
-const ListProducts = ({products, categories, category, setCategory, isAdmin}) => {
-  const [descriptionFormsOpen, setDescriptionFormsOpen] = useState(products.reduce((acc, _, index) => ({ ...acc, [`product-${index}`]: false }), {}));
+const ListProducts = ({products, categories, category, setCategory}) => {
   products = products.filter(product => category == 0 || product.CategoryId == category);
   return (
     <>
     <CategorySelect categories={categories} category={category} setCategory={setCategory} />
-    {products.map((product, index) => {
-      return (
-        <div key={`product-${index}`}>
-          <Link key={product.id} to={`/products/${product.id}`}><ProductRow product={product} showDescription={isAdmin}></ProductRow></Link>
-          {isAdmin && <button onClick={() => {setDescriptionFormsOpen(state => ({ ...state, [`product-${index}`]: true }))}}>Change Description</button>}
-          {descriptionFormsOpen[`product-${index}`] && (
-            <form onSubmit={async (event) => {
-              event.preventDefault();
-              const form = event.target;
-
-              const data = { ...product, description: form.description.value };
-              const response = await fetch(`http://localhost:3001/api/products/${product.id}`, {
-                  method: 'put',
-                  headers: { "Content-Type": "application/json" },
-                  body: JSON.stringify(data)
-              });
-              if (response.ok) {
-                setDescriptionFormsOpen(state => ({ ...state, [`product-${index}`]: false }))
-              }
-            }}>
-              <label htmlFor={`item-${product.id}`}>Add a new decription:</label>
-              <input id={`item-${product.id}`} name="description" type="text"/>
-              <input type="submit" value="Update description"/>
-            </form>
-          )}
-        </div>
-      );
-      
+    {products.map(product => {
+      return <Link key={product.id} to={`/products/${product.id}`}><ProductRow product={product}></ProductRow></Link>
     })
     }
     </>
