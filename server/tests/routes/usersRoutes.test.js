8805331083588c1908c22a99de612edc8d7df894
Ross Keeley <ross.keeley@sky.uk>
2021-11-12 13:34:38 +0000

    Added tests for usersRoutes

--- server/tests/routes/usersRoutes.test.js
+++ server/tests/routes/usersRoutes.test.js
@@ -1,13 +1,13 @@
-const supertest = require('supertest');
-const { sequelize, User } = require("../../connect");
-// const { User }  = require("../../routes/usersRoutes");
+const supertest = require("supertest");
+const request = require("supertest");
+const { sequelize } = require("../../connect");
 const app = require("../../index");
 const api = supertest(app);
-const { hasUncaughtExceptionCaptureCallback } = require("process");
 
 
 beforeAll(async () => {
     await sequelize.sync({
+        force: true,
         logging: false,
     });
 })
@@ -23,55 +23,71 @@ describe("GET", () => {
     })
 })
 
-describe("POST", () => {
-    test("New user is created", async () => {
-        // const user = {
-        //     "username": "username",
-        //     "name": "name",
-        //     "password": null,
-        //     "isAdmin": false
-        // }
-        try {
-            let post = await api.post("/api/users");
-            await api
-            .post("/api/users")
-            .expect(201)
-            .expect("Content-Type", /application\/json/)
-            // const count = await User.count();
-            // await api.post("/api/users").send(user)
-            // const newCount = await User.count()
-            // expect(newCount).toBe(count + 1);
-            // await request(app)
-            // .post("/api/users")
-            // .send()
-            
-        } catch (err) {
-            console.log(`Post Test ${err}`)
-        }
+describe("DELETE by id", () => {
+    test("Delete a user", async () => {
+        const response = await request(app).post("/api/users").send({
+            "username": "testGetId",
+            "name": "name",
+            "password": null,
+            "isAdmin": false
+        })
+        const deleteItem = await request(app).delete("/api/users/" + response.body.id)
+        expect(deleteItem.statusCode).toBe(200)
+        const getById = await request(app).get("/api/users/" + response.body.id)
+        expect(getById.statusCode).toBe(404)
+        
     })
 })
+
+describe("POST", () => {
+    test("should respond with a 201 status code", async () => {
+        const response = await request(app).post("/api/users").send({
+            "username": "testRespond201",
+            "name": "name",
+            "password": null,
+            "isAdmin": false
+        })
+        expect(response.statusCode).toBe(201)
+        await request(app).delete("/api/users/" + response.body.id)
+    })
+    
+    test("should return json in the content type header", async () => {
+        const response = await request(app).post("/api/users").send({
+            "username": "testReturnJSON",
+            "name": "name",
+            "password": null,
+            "isAdmin": false
+        })
+        expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"))
+        await request(app).delete("/api/users/" + response.body.id)
+    })
+
+    test("response object contains an ID", async () => {
+        const response = await request(app).post("/api/users").send({
+            "username": "testContainsId",
+            "name": "name",
+            "password": null,
+            "isAdmin": false
+        })
+        expect(response.body.id).toBeDefined()
+        await request(app).delete("/api/users/" + response.body.id)    
+    })
+})
+
 
 describe("PUT", () => {
     test("User has been successfully updated", async () => {
-        try {
-
-        } catch (err) {
-            console.log(`Put Test: ${err}`)
-        }
-    })
-})
-
-describe("User", () => {
-    beforeAll(async () => {
-        await sequelize.sync({ force: true });
-    })
-
-    test("can create a user", async () => {
-        const user = await User.create({ 
-            username: "TestyMcTestface", 
-            name: "Danny Ric", 
-            password: null, 
-            isAdmin: false 
-        });
+        const response = await request(app).post("/api/users").send({
+            "username": "testUpdateUser",
+            "name": "name",
+            "password": null,
+            "isAdmin": false
+        })
+        const UpdateUser = await request(app).put("/api/users/" + response.body.id).send({
+            username: "UpdatedUserName"
+        })
+        console.log(UpdateUser)
+        expect(UpdateUser.body.username).toBe("UpdatedUserName")
+        await request(app).delete("/api/users/" + response.body.id)   
     })
 })
